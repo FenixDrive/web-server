a697e76fd7c247c4c2552cb49098c7378c2b86ff
FenixDrive <davidbsmart@hotmail.com>
2016-11-06 16:28:41 +1300

    Back to old files

--- middleware.js
+++ middleware.js
@@ -1,14 +1,15 @@
-// Use module exports to expose middleware
+//Uses request and response. RequireAuthentication runs first. Next then tells the call to move on. 
+var middleware = { 
+	requireAuthentication: function (req, res, next) { 
+			console.log('private route hit!');
+			next();
 
-var middleware = {
-	requireAuthentication: function (req, res, next) {
-		console.log('private route hit!');
-		next();
 	},
 	logger: function (req, res, next) {
-		console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
-		next();
+			console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+			next();
 	}
 };
+
 
 module.exports = middleware;
